20bd5b99cb1fd66bf09c2b6211146f96595633c9
unknown <apodworny@ITSM1033878.ACADEMIC.RRC.CA>
2012-11-21 20:00:26 -0600

    added a 'Type' model and used the 'rake db:migrate' command

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121122010842) do
+ActiveRecord::Schema.define(:version => 20121122015938) do
+
+  create_table "types", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "weapons", :force => true do |t|
     t.string   "name"
