6a9f175d3413455025892e507d880ba702159115
unknown <apodworny@ITSM1033878.ACADEMIC.RRC.CA>
2012-11-21 19:16:36 -0600

    Added the default route to go to 'weapons#index'

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Store::Application.routes.draw do
   resources :weapons
+  
+  root :to => "weapons#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
