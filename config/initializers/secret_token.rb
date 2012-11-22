15f21a6e5dd1f2661cc37adadfc0049199bfc6a3
unknown <apodworny@ITSM1033878.ACADEMIC.RRC.CA>
2012-11-21 19:06:41 -0600

    Used command: 'rails new store' and added all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'da88e2d9f9cd54b43d0913632f8d9abaa94bb420df585186c1ae354a435cf92efdf7c80f7da6868c06ef767b8ac1e268bafa3cd75fa4a10c377d62557198537d'
